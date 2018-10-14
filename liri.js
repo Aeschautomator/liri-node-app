fcde251f8d95f5ed803a30bec1c6932e4846c511
Alex esch <eschmassage@gmail.com>
2018-10-14 16:02:00 -0400

    installed dotenv packages

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 var keys = require('./keys.js');
 var request = require('request');
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
 var fs = require('fs');
 
 // Stored arguement's array
